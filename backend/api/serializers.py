0e3df5479f595087decb78056a79a8c08a08398e
Андрей Подгородецкий <flash5z@yandex.com>
2026-06-11 15:56:32 +0400

    Implement users API and subscriptions

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
+from drf_extra_fields.fields import Base64ImageField
 
-from recipes.models import Tag, Ingredient
+from recipes.models import Tag, Ingredient, Recipe
 from users.models import User
 
 
@@ -36,3 +37,89 @@ class UserSerializer(serializers.ModelSerializer):
         if request is None or request.user.is_anonymous:
             return False
         return obj.subscribers.filter(user=request.user).exists()
+
+
+class UserCreateSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = (
+            'email',
+            'username',
+            'first_name',
+            'last_name',
+            'password',
+        )
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+
+
+class SetPasswordSerializer(serializers.Serializer):
+    current_password = serializers.CharField(
+        write_only=True
+    )
+    new_password = serializers.CharField(
+        write_only=True
+    )
+
+    def validate_new_password(self, value):
+        user = self.context['request'].user
+        if not user.check_password(value):
+            return value
+        raise serializers.ValidationError('New password must be different from the old one.')
+
+
+class SetAvatarSerializer(serializers.Serializer):
+    avatar = Base64ImageField()
+
+
+class AvatarResponseSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('avatar',)
+
+
+class RecipeMinifiedSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Recipe
+        fields = (
+            'id',
+            'name',
+            'image',
+            'cooking_time',
+        )
+
+
+class UserWithRecipesSerializer(UserSerializer):
+    recipes = serializers.SerializerMethodField()
+    recipes_count = serializers.SerializerMethodField()
+
+    class Meta(UserSerializer.Meta):
+        fields = UserSerializer.Meta.fields + (
+            'recipes',
+            'recipes_count',
+        )
+
+    def get_recipes(self, obj):
+        request = self.context.get('request')
+        if request is None:
+            return []
+
+        recipes_limit = request.query_params.get('recipes_limit')
+        recipes_qs = obj.recipes.all()
+
+        if recipes_limit is not None and recipes_limit.isdigit():
+            recipes_qs = recipes_qs[:int(recipes_limit)]
+
+        return RecipeMinifiedSerializer(
+            recipes_qs,
+            many=True
+        ).data
+
+    def get_recipes_count(self, obj):
+        return obj.recipes.count()
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,19 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status, mixins
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-from .serializers import TagSerializer, IngredientSerializer, UserSerializer
+from django.shortcuts import get_object_or_404
+
+from .serializers import (TagSerializer,
+                          IngredientSerializer,
+                          UserSerializer,
+                          UserCreateSerializer,
+                          SetPasswordSerializer,
+                          SetAvatarSerializer,
+                          AvatarResponseSerializer,
+                          UserWithRecipesSerializer)
 from recipes.models import Tag, Ingredient
-from users.models import User
+from users.models import User, Subscription 
 
 
 class TagViewSet(viewsets.ReadOnlyModelViewSet):
@@ -24,6 +35,160 @@ class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
         return Ingredient.objects.all()
 
 
-class UserViewSet(viewsets.ReadOnlyModelViewSet):
+class UserViewSet(
+        mixins.ListModelMixin,
+        mixins.RetrieveModelMixin,
+        mixins.CreateModelMixin,
+        viewsets.GenericViewSet
+    ):
     queryset = User.objects.all()
-    serializer_class = UserSerializer
+
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return UserCreateSerializer
+        return UserSerializer
+    
+    @action(detail=False, methods=['get'])
+    def me(self, request):
+        serializer = UserSerializer(request.user, context={'request': request})
+        return Response(serializer.data)
+    
+    @action(detail=False, methods=['post'])
+    def set_password(self, request):
+        serializer = SetPasswordSerializer(
+            data=request.data,
+            context={'request': request}
+        )
+        serializer.is_valid(raise_exception=True)
+
+        current_password = serializer.validated_data['current_password']
+        new_password = serializer.validated_data['new_password']
+
+        if not request.user.check_password(current_password):
+            return Response(
+                {'current_password': 'Неверный текущий пароль.'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        request.user.set_password(new_password)
+        request.user.save()
+
+        return Response(
+            status=status.HTTP_204_NO_CONTENT
+        )
+    
+    @action(
+        detail=False,
+        methods=['put'],
+        url_path='me/avatar'
+    )
+    def avatar(self, request):
+        serializer = SetAvatarSerializer(
+            data=request.data,
+            context={'request': request}
+        )
+        serializer.is_valid(raise_exception=True)
+
+        request.user.avatar = serializer.validated_data['avatar']
+        request.user.save()
+
+        return Response(
+            AvatarResponseSerializer(request.user).data
+        )
+    
+    @avatar.mapping.delete
+    def delete_avatar(self, request):
+        request.user.avatar.delete(save=False)
+        request.user.avatar = None
+        request.user.save()
+
+        return Response(
+            status=status.HTTP_204_NO_CONTENT
+        )
+    
+    @action(
+        detail=True,
+        methods=['post']
+    )
+    def subscribe(self, request, pk=None):
+        author = get_object_or_404(
+            User,
+            pk=pk
+        )
+
+        if request.user == author:
+            return Response(
+                {'errors': 'Нельзя подписаться на самого себя'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        if Subscription.objects.filter(
+            user=request.user,
+            author=author
+        ).exists():
+            return Response(
+                {'errors': 'Вы уже подписаны'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        
+        Subscription.objects.create(
+            user=request.user,
+            author=author
+        )
+
+        serializer = UserWithRecipesSerializer(
+            author,
+            context={'request': request}
+        )
+
+        return Response(
+            serializer.data,
+            status=status.HTTP_201_CREATED
+        )
+    
+    @subscribe.mapping.delete
+    def unsubscribe(self, request, pk=None):
+        author = get_object_or_404(
+            User,
+            pk=pk
+        )
+
+        subscription = Subscription.objects.filter(
+            user=request.user,
+            author=author
+        ).first()
+
+        if subscription is None:
+            return Response(
+                {'errors': 'Подписка не найдена'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        subscription.delete()
+
+        return Response(
+            status=status.HTTP_204_NO_CONTENT
+        )
+    
+    @action(
+        detail=False,
+        methods=['get']
+    )
+    def subscriptions(self, request):
+        subscriptions = Subscription.objects.filter(
+            user=request.user
+        ).select_related('author')
+
+        authors = [sub.author for sub in subscriptions]
+
+        page = self.paginate_queryset(authors)
+
+        serializer = UserWithRecipesSerializer(
+            authors,
+            many=True,
+            context={'request': request}
+        )
+
+        return self.get_paginated_response(
+            serializer.data
+        )
